2975f4bb9bb783f149891b12f55ce2b86a286002
Krzysztof Boduch <krzysztof.boduch@koffeecup.net>
2020-04-10 22:21:42 +0200

    Improved RoomPosition isSteppable
    consider wall tile steppable if there is a road

--- src/main/kotlin/starter/RoomPosition.kt
+++ src/main/kotlin/starter/RoomPosition.kt
@@ -164,7 +164,7 @@ fun RoomPosition.getTerrainAt(): TerrainConstant {
 }
 
 fun RoomPosition.isSteppable(includeCreeps: Boolean = true, includeStructures: Boolean = true): Boolean {
-    if (getTerrainAt() == TERRAIN_WALL) {
+    if (getTerrainAt() == TERRAIN_WALL && lookFor(LOOK_STRUCTURES)?.none { it.structureType == STRUCTURE_ROAD } == true) {
         return false
     }
 
